f4e2e10b2e50db149ae2411cd01cc68a3d78ef8e
camel <jys5540@gmail.com>
2025-06-16 14:24:12 +0900

    Delete

--- .next/server/next-font-manifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":false}';
-=======
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
->>>>>>> 19e7c56f2809992574ac1a385cf1ed49fa1b3333
