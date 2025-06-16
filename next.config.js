6145ad306005a93ab703ef19be43f8da33ad9979
camel <jys5540@gmail.com>
2025-06-16 14:50:20 +0900

    Config 수정

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // GitHub Pages 리포지토리 이름과 동일하게 설정
-  basePath: process.env.NODE_ENV === 'production' ? '/realtime-metro' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/realtime-metro/' : '',
+  // GitHub Pages 리포지토리 이름과 정확히 일치해야 합니다.
+  basePath: '/realtime-metro', // GitHub 리포지토리 이름
+  assetPrefix: '/realtime-metro/', // basePath와 동일하게 설정하고 슬래시(/)로 끝나야 합니다.
+  images: {
+    unoptimized: true, // 정적 빌드 시 이미지 최적화 비활성화 (필요하다면)
+  },
 };
 
 module.exports = nextConfig;
